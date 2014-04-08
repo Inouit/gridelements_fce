c03e0048ac5dfb4e69d8bcb2ffb206f047f6bc90
Grégory Copin <gcopin@inouit.com>
2014-04-08 09:54:33 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+Gridelements_fce
+================
+
+This extension provide some hooks to make flexible content elemenents easier with gridelements typo3 extension
+
+Requirements
+============
+
+ - Typo3 6.2 or higher
+ - Gridelements typo3 extension must be installed
---
+++ ext_autoload.php
@@ -0,0 +1,8 @@
+<?php
+$extensionClassesPath = t3lib_extMgm::extPath('gridelements_fce') . 'Classes/';
+
+return array(
+  'Inouit\gridelementsFce\Hooks\DrawItem' => $extensionClassesPath . 'Hooks/DrawItem.php',
+  'Inouit\gridelementsFce\Hooks\CObj' => $extensionClassesPath . 'Hooks/CObj.php',
+  'Inouit\gridelementsFce\Hooks\GetData' => $extensionClassesPath . 'Hooks/GetData.php',
+);
